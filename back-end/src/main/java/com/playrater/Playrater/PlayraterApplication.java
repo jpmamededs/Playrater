221bc835c01db8337787f8153fae0b88521881b5
jpmamededs <joaopedrodias@live.com>
2025-03-30 11:51:58 -0300

    feat: Spring Boot back-end

---
+++ back-end/src/main/java/com/playrater/Playrater/PlayraterApplication.java
@@ -0,0 +1,13 @@
+package com.playrater.Playrater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayraterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayraterApplication.class, args);
+	}
+
+}
