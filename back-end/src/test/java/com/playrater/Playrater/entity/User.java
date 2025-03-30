714ae8a6970ee05792230dc2169373ada73fae6b
jpmamededs <joaopedrodias@live.com>
2025-03-30 12:17:38 -0300

    feat: User entity data

---
+++ back-end/src/test/java/com/playrater/Playrater/entity/User.java
@@ -0,0 +1,16 @@
+package com.playrater.Playrater.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+  private int id;
+  private String username;
+  private String name;
+  private String email;
+  private int age;
+  private String gender;
+
+
+}
